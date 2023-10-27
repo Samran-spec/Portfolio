59cd2cf5c9aec2598ca1fd56f57e46b0ee5ee250
Samran Haider <s4haider@uwaterloo.ca>
2023-10-27 18:32:15 -0400

    till hero

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,9 +1,24 @@
 import './HeroImgStyle.css'
 import React from 'react'
+import bgImg  from '../assets/bg-img.jpg'
+import {Link} from 'react-router-dom'
 
 const HeroImg = () => {
   return (
-    <div>HeroImg</div>
+    <div className='hero'>
+      <div className='mask'>
+        <img className='into-img' src={bgImg} alt='bg-img'/>
+      </div>
+      <div className='content'>
+        <p>hi, i'm a</p>
+        <h1>Full Stack Developer.</h1>
+        <div>
+          <Link to='/project' className='btn'>Projects</Link>
+          <Link to='/contact' className='btn-light'>Contact</Link>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
